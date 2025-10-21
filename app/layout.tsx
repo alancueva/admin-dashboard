bcfde1f220035928289c0b239f307b65c591efd1
alancuevat <alancuevat@gmail.com>
2025-10-21 16:29:22 -0500

    nuevo nombre del sitema en los metadas

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'Control de Stock',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'Administra tus productos y controla el stock de manera eficiente.',
 };
 
 export default function RootLayout({
