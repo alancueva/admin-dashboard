abe7ff9c8560caff27fb008209dbe9dfa5d5ba2d
alancuevat <alancuevat@gmail.com>
2025-10-27 17:56:16 -0500

    otras ventanas de administracion para dar comienzo...

---
+++ app/(dashboard)/Administracion/organizaciones/page.tsx
@@ -0,0 +1,21 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+    CardFooter
+} from '@/components/ui/card';
+
+export default function OrganizacionPage() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Organización</CardTitle>
+                <CardDescription>Organización..</CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+
+        </Card>
+    );
+}
---
+++ app/(dashboard)/Administracion/tipos-negocio/page.tsx
@@ -0,0 +1,21 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+    CardFooter
+} from '@/components/ui/card';
+
+export default function TipoNegocioPage() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Tipo de Negocio</CardTitle>
+                <CardDescription>Tipo de negocio ....</CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+
+        </Card>
+    );
+}
