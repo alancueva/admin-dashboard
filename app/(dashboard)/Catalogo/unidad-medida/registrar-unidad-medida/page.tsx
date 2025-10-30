45d354b5d501f5859abd2e7bd142b323b3bc1606
alancuevat <alancuevat@gmail.com>
2025-10-30 15:00:33 -0500

    modulo de unidad de medida con su conuslta, registrar, modificar

---
+++ app/(dashboard)/Catalogo/unidad-medida/registrar-unidad-medida/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import {
+    Home,
+    PlusCircle,
+    Package,
+    Package2,
+    PanelLeft,
+    Settings,
+    ShoppingCart,
+    Users2,
+    ArrowLeftRight,
+    Check,
+    ShieldPlus,
+    SlidersVertical
+} from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+export default function RegistrarUnidadMedida() {
+    const tipo: any[] = [
+        { value: 'cantidad', label: 'cantidad' },
+        { value: 'peso', label: 'peso' },
+        { value: 'volumen', label: 'volumen' },
+        { value: 'longitud', label: 'longitud' }
+    ]
+    return (
+        <div>
+            <Card className="sticky top-0 z-50 mb-4">
+                <CardHeader>
+                    <div className="flex items-center gap-2">
+                        <CardTitle>Registrar Unidad de Medida</CardTitle>
+
+                        <div className="ml-auto flex items-center gap-2">
+                            <Button
+                                size="sm"
+                                className="h-8 gap-1"
+                            >
+                                <Check className="h-3.5 w-3.5" />
+                                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                    grabar
+                                </span>
+                            </Button>
+
+                            <Link href="/Catalogo/unidad-medida">
+                                <Button
+                                    size="sm"
+                                    className="h-8 gap-1"
+                                >
+                                    <ArrowLeftRight className="h-3.5 w-3.5" />
+                                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                        Volver
+                                    </span>
+                                </Button>
+                            </Link>
+                        </div>
+                    </div>
+                </CardHeader>
+            </Card>
+
+            <Card>
+                <CardContent>
+                    <form className="grid grid-cols-12 gap-4 mt-2">
+                        {/* Nombre  */}
+                        <div className="col-span-12 md:col-span-4">
+                            <label className="block mb-1 font-medium">
+                                Nombres de Unidad de Medida
+                            </label>
+                            <Input
+                                type="text"
+                                placeholder="Nombres de Unidad de Medida"
+                                className="w-full border border-gray-300 rounded-md p-2"
+                            />
+                        </div>
+                        <div className="col-span-12 md:col-span-4">
+                            <label className="block mb-1 font-medium">
+                                Abreviatura
+                            </label>
+                            <Input
+                                type="text"
+                                placeholder="Abreviatura"
+                                className="w-full border border-gray-300 rounded-md p-2"
+                            />
+                        </div>
+
+                        <div className="col-span-12 md:col-span-4">
+                            <label className="block mb-1 font-medium">
+                                Tipo
+                            </label>
+                            <select className="w-full border border-gray-300 rounded-md p-2">
+                                <option value="">Seleccione</option>
+                                {tipo.map((t) => (
+                                    <option key={t.value} value={t.value}>
+                                        {t.label}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+
+
+
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
---
+++ app/(dashboard)/Catalogo/unidad-medida/unidad-table.tsx
@@ -0,0 +1,139 @@
+'use client';
+
+import { TableHead, TableRow, TableHeader, TableBody, Table } from '@/components/ui/table';
+import {
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
+import { Unidad } from './unidad';
+import { useRouter } from 'next/navigation';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+
+export function UnidadTable({
+    unidad,
+    offset,
+    totalUnidad
+}: {
+    unidad: any[];
+    offset: number;
+    totalUnidad: number;
+}) {
+    let router = useRouter();
+    let unidadPerPage = 5;
+
+    function prevPage() {
+        router.back();
+    }
+
+    function nextPage() {
+        router.push(`/?offset=${offset}`, { scroll: false });
+    }
+
+    const [selectedStatus, setSelectedStatus] = useState('Todos');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [records, setRecords] = useState(unidad);
+
+    const handleFilter = (status: string, search: string) => {
+        const lowercasedSearch = search.toLowerCase();
+        const filteredData = unidad.filter(user => {
+            const matchesSearch = user.nombres.toLowerCase().includes(lowercasedSearch) ||
+                user.abreviatura.toLowerCase().includes(lowercasedSearch) ||
+                user.tipo.toLowerCase().includes(lowercasedSearch);
+            return matchesSearch;
+        });
+        setRecords(filteredData);
+    };
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newSearchTerm = e.target.value;
+        setSearchTerm(newSearchTerm);
+        handleFilter(selectedStatus, newSearchTerm);
+    }
+
+    return (
+        <div>
+            <CardHeader>
+                <div className='grid grid-cols-12 gap-4 mt-2'>
+                    <div className="col-span-12 md:col-span-4">
+                        <label className="block mb-1 font-medium">
+                            Unidad de Medida
+                        </label>
+                        <Input
+                            type="text"
+                            placeholder="Buscar Unidad de Medida..."
+                            className="w-full border border-gray-300 rounded-md p-2"
+                            onChange={handleSearchChange}
+                        />
+                    </div>
+                    
+                </div>
+            </CardHeader>
+
+            <CardContent>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Nombres</TableHead>
+                            <TableHead>Abreviatura</TableHead>
+                            <TableHead className="hidden md:table-cell">Tipo</TableHead>
+                            <TableHead>
+                                <span className="sr-only">Acciones</span>
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {records.map((user) => (
+                            <Unidad key={user.id} uni={user} />
+                        ))}
+                    </TableBody>
+                </Table>
+            </CardContent>
+
+            <CardFooter>
+                <form className="flex items-center w-full justify-between">
+                    <div className="text-xs text-muted-foreground">
+                        Mostrando {' '}
+                        <strong>
+                            {Math.max(0, Math.min(offset - unidadPerPage, totalUnidad) + 1)}-{offset}
+                        </strong>{' '}
+                        De <strong>{totalUnidad}</strong> Usuarios
+                    </div>
+                    <div className="flex">
+                        <Button
+                            formAction={prevPage}
+                            variant="ghost"
+                            size="sm"
+                            type="submit"
+                            disabled={offset === unidadPerPage}
+                        >
+                            <ChevronLeft className="mr-2 h-4 w-4" />
+                            Anterior
+                        </Button>
+                        <Button
+                            formAction={nextPage}
+                            variant="ghost"
+                            size="sm"
+                            type="submit"
+                            disabled={offset + unidadPerPage > totalUnidad}
+                        >
+                            Siguiente
+                            <ChevronRight className="ml-2 h-4 w-4" />
+                        </Button>
+                    </div>
+                </form>
+            </CardFooter>
+        </div>
+    );
+}
