a4529bc80dc33db102169060c50347e15a68e447
alancuevat <alancuevat@gmail.com>
2025-12-10 16:47:30 -0500

    ahora el page principal solo el muestra el inicio del dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,20 +1,11 @@
-'use client';
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { File, PlusCircle } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CardFooter
-} from '@/components/ui/card';
 import Dashboard from './Inicio/page';
-export default function ProductsPage() {
+
+export default function Page() {
+
   return (
-    <Dashboard />
-  );
+    <>
+      <Dashboard/>
+    </>
+  )
 }
