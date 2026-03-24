735346017efac6caaaed40047d9b991427aa1631
alancuevat <alancuevat@gmail.com>
2026-03-24 10:52:40 -0500

    cambio de categoria, quitando el nivel, agregando la vigencia, tambien en el modificar agregar el id y de igual manera con sus campos

--- app/dashboard/Catalogo/categoria/registrar-categoria/page.tsx
+++ app/dashboard/Catalogo/categoria/registrar-categoria/page.tsx
@@ -1,26 +1,12 @@
-"Use client";
+'Use client';
 
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import {
-  Home,
-  PlusCircle,
-  Package,
-  Package2,
-  PanelLeft,
-  Settings,
-  ShoppingCart,
-  Users2,
-  ArrowLeftRight,
-  Check,
-  ShieldPlus,
-  SlidersVertical
-} from 'lucide-react';
+import { ArrowLeftRight, Check } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function RegistrarCategoria() {
-
   return (
     <div>
       <Card className="sticky top-0 z-50 mb-4">
@@ -29,10 +15,7 @@ export default function RegistrarCategoria() {
             <CardTitle>Registrar Categoria</CardTitle>
 
             <div className="ml-auto flex items-center gap-2">
-              <Button
-                size="sm"
-                className="h-8 gap-1"
-              >
+              <Button size="sm" className="h-8 gap-1">
                 <Check className="h-3.5 w-3.5" />
                 <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                   grabar
@@ -40,10 +23,7 @@ export default function RegistrarCategoria() {
               </Button>
 
               <Link href="/dashboard/Catalogo/categoria">
-                <Button
-                  size="sm"
-                  className="h-8 gap-1"
-                >
+                <Button size="sm" className="h-8 gap-1">
                   <ArrowLeftRight className="h-3.5 w-3.5" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                     Volver
@@ -60,51 +40,41 @@ export default function RegistrarCategoria() {
           <form className="grid grid-cols-12 gap-4 mt-2">
             {/* Nombre del categoria */}
             <div className="col-span-12 md:col-span-4">
-              <label className="block mb-1 font-medium">
-                Nombres
-              </label>
+              <label className="block mb-1 font-medium">Nombres</label>
               <Input
                 type="text"
                 placeholder="Nombres de categoria"
                 className="w-full border border-gray-300 rounded-md p-2"
               />
             </div>
-            <div className="col-span-12 md:col-span-8">
-            </div>
+            <div className="col-span-12 md:col-span-8"></div>
             <div className="col-span-12 md:col-span-12">
-              <label className="block mb-1 font-medium">
-                Descripción
-              </label>
+              <label className="block mb-1 font-medium">Descripción</label>
               <Input
                 type="text"
                 placeholder="Descripción"
                 className="w-full border border-gray-300 rounded-md p-2"
               />
             </div>
-            
             <div className="col-span-12 md:col-span-2">
-              <label className="block mb-1 font-medium">
-                Nivel
-              </label>
+              <label className="block mb-1 font-medium">Orden</label>
               <Input
                 type="text"
                 placeholder="0"
                 className="w-full border border-gray-300 rounded-md p-2"
               />
             </div>
-
             <div className="col-span-12 md:col-span-2">
-              <label className="block mb-1 font-medium">
-                Orden
-              </label>
-              <Input
-                type="text"
-                placeholder="0"
+              <label className="block mb-1 font-medium">Vigencia</label>
+              <select
                 className="w-full border border-gray-300 rounded-md p-2"
-              />
+                disabled={true}
+                defaultValue="SI"
+              >
+                <option value="SI">SI</option>
+                <option value="NO">NO</option>
+              </select>
             </div>
-
-            
           </form>
         </CardContent>
       </Card>
