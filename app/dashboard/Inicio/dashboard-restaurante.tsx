8abf1676c003d6d32e47d5d32c0309bb6d21dae4
alancuevat <alancuevat@gmail.com>
2026-06-17 14:42:31 -0500

    falto poner en el anterior commit el agregado de title para el boton

--- app/dashboard/Inicio/dashboard-restaurante.tsx
+++ app/dashboard/Inicio/dashboard-restaurante.tsx
@@ -72,14 +72,20 @@ export default function DashboardRestaurante() {
         </div>
         <div className="flex justify-between  gap-2">
           <Link href="/dashboard/operaciones-de-servicio/pedidos">
-            <Button className="flex items-center gap-2 bg-primary hover:bg-primary/90 text-white px-5 py-2.5 rounded-xl">
+            <Button
+              className="flex items-center gap-2 bg-primary hover:bg-primary/90 text-white px-5 py-2.5 rounded-xl"
+              title="Toma de Pedido"
+            >
               <ClipboardList className="w-5 h-5" />
               Pedido
             </Button>
           </Link>
 
           <Link href="/dashboard/operaciones-de-servicio/monitor-de-cocina">
-            <Button className="flex items-center gap-2 bg-primary hover:bg-primary/90 text-white px-5 py-2.5 rounded-xl">
+            <Button
+              className="flex items-center gap-2 bg-primary hover:bg-primary/90 text-white px-5 py-2.5 rounded-xl"
+              title="Monitor de Cocina"
+            >
               <Cookie className="w-5 h-5" />
               Cocina
             </Button>
